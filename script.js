9164eca6e2fb6c128571c1405d78126518768240
Manu Suhonen <manu.suhonen@gmail.com>
2024-09-24 18:09:48 +0300

    Initial rewrite of implementing game in UI

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 console.log("LOADED");
 
 let humanScore = 0, compScore = 0;
-let gamecount = { current: 1, max: 5 };
+
+let gamecount = { current: 0, max: 5 };
 let gameCountMsg = "";
+
+console.clear()
+
+let btns = document.querySelectorAll("button");
+
+let body = document.querySelector("body");
+
+let scoreboardStr = "";
 
 const choices = [
     { val: "ROCK", weakness: "PAPER" },
@@ -29,42 +38,78 @@ function playRound(humanChoice, computerChoice) {
     console.log(`You chose: ${humanChoice}`)
     console.log(`Computer chose: ${computerChoice.val}`)
 
-    let msg = gameCountMsg + `\n\nYou chose: ${humanChoice}\nComputer chose: ${computerChoice.val}`;
+    let userSelection = document.querySelector("#user-selection");
+    let roboSelection = document.querySelector("#machine-selection");
+    let scoreboard = document.querySelector("#score-comp");
+
+    userSelection.textContent = `You chose: ${humanChoice}`;
+    roboSelection.textContent = `Computer chose: ${computerChoice.val}`;
+
+
+    let msg = gameCountMsg + `\n\nYou choooooose: ${humanChoice}\nComputer chose: ${computerChoice.val}`;
 
     if (humanChoice === computerChoice.val) {
         console.log("draw");
-        alert(msg + "\n\n" + "DRAW!!!!!");
+        //alert(msg + "\n\n" + "DRAW!!!!!");
     } else {
         if (computerChoice.weakness === humanChoice) {
             console.log("You Win!!!");
-            alert(msg + "\n\n" + "You Win!!!");
+            //alert(msg + "\n\n" + "You Win!!!");
             humanScore++;
         } else {
             console.log("comp win");
-            alert(msg + "\n\n" + "You Lose :( :( :(");
+            //alert(msg + "\n\n" + "You Lose :( :( :(");
             compScore++;
         }
+    }
+
+    let finalMsg = `Your score: ${humanScore}\nComputer score: ${compScore}`;
+
+    console.log(finalMsg);
+
+    scoreboard.textContent = finalMsg;
+
+    gamecount.current++;
+
+    console.log(gamecount.current);
+
+    if (gamecount.current == gamecount.max) {
+        alert("game ended....");
     }
 }
 
 function playGame() {
 
-    for (; gamecount.current <= gamecount.max; gamecount.current++) {
-        gameCountMsg = `Current Game: ${gamecount.current} of ${gamecount.max}`;
-        playRound(getHumanChoice(), getComputerChoice());
-    }
+    // for (; gamecount.current <= gamecount.max; gamecount.current++) {
+    //     gameCountMsg = `Current Game: ${gamecount.current} of ${gamecount.max}`;
+    //     playRound(getHumanChoice(), getComputerChoice());
+    // }
 
-    let finalMsg = `Your score: ${humanScore}\nComputer score: ${compScore}`;
 
-    if (humanScore === compScore) {
-        alert(finalMsg + "\n\n" + "Game is a Draw...");
-    } else {
-        if (humanScore > compScore) {
-            alert(finalMsg + "\n\n" + "You Win!!!");
-        } else {
-            alert(finalMsg + "\n\n" + "You Lose :( :( :(");
-        }
-    }
+
+    // if (humanScore === compScore) {
+    //     alert(finalMsg + "\n\n" + "Game is a Draw...");
+    // } else {
+    //     if (humanScore > compScore) {
+    //         alert(finalMsg + "\n\n" + "You Win!!!");
+    //     } else {
+    //         alert(finalMsg + "\n\n" + "You Lose :( :( :(");
+    //     }
+    // }
 }
 
-playGame();
+//playGame();
+
+btns.forEach(btn => {
+    console.log(`Adding listeners`);
+
+    btn.onclick = () => {
+        playRound(btn.textContent, getComputerChoice());
+    };
+})
+
+
+// console.log(`Computer chose: ${getComputerChoice().val}`)
+// console.log(`You chose: ${btn.textContent}`)
+//let robo = `Computer chose: ${getComputerChoice().val}`;
+//let human = `You chose: ${btn.textContent}`;
